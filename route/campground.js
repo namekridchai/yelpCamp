9dbe4404d2a78c24d1e42e444e3bed95bdc3b557
namekridchai <kridchai.1@gmail.com>
2022-12-19 23:29:39 +0700

    star feature
    clean code for route

--- route/campground.js
+++ route/campground.js
@@ -6,19 +6,20 @@ const app = express();
 const campground = require('../controller/campground')
 const router = express.Router({mergeParams:true});
 
+router.route('/')
+      .get(campground.index)
+      .post( isLogin,isAuthor,validateCampground,catchAsync( campground.createCampground))
+  
+router.get('/new',isLogin,isAuthor,campground.newForm);
+router.route('/:id')
+       .get(catchAsync(campground.showCampground))
+       .put(isLogin,isAuthor,catchAsync(campground.update))
+       .delete(isLogin,isAuthor,catchAsync(campground.delete))
+  
+router.get('/:id/edit',isLogin,isAuthor,catchAsync(campground.renderEditForm));
+  
 
-router.get('/',campground.index);
   
-  router.get('/new',isLogin,isAuthor,campground.newForm);
   
-  router.get('/:id',catchAsync(campground.showCampground));
-  
-  router.post('/', isLogin,isAuthor,validateCampground,catchAsync( campground.createCampground));
-  
-  router.get('/:id/edit',isLogin,isAuthor,catchAsync(campground.renderEditForm));
-  
-  router.put('/:id',isLogin,isAuthor,catchAsync(campground.update));
-  
-  router.delete('/:id',isLogin,isAuthor,catchAsync(campground.delete))
 
   module.exports = router
