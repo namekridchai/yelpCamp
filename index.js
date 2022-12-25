eb8ea736c8fed16439d341fa260b1a76a7eece37
namekridchai <kridchai.1@gmail.com>
2022-12-25 21:29:35 +0700

    final
    final

--- index.js
+++ index.js
@@ -18,7 +18,19 @@ const flash  = require('connect-flash')
 const user = require('./models/user')
 const local = require('passport-local')
 const passport = require('passport')
+const MongoStore = require('connect-mongo');
+const atlas = process.env.atlas||'mongodb://127.0.0.1:27017/yelp'
+const url = 'mongodb://127.0.0.1:27017/yelp'
+const store = new MongoStore({
+  mongoUrl: atlas,
+  secret:'secret',
+  touchAfter: 24 * 60 * 60
+});
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e)
+})
 const configSession = {
+  store,
   secret:'secret'
   ,resave:false,
   saveUninitialized:true,
@@ -30,12 +42,62 @@ const configSession = {
     
   }
 }
-
+const mongoSanitize = require('express-mongo-sanitize');
 const routeCamp = require('./route/campground')
 const routeReview = require('./route/review');
 const routeUser = require('./route/users');
+const helmet = require("helmet");
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/dyjryngbn/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+              "https://images.unsplash.com/",
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+);
 
-
+app.use(
+  mongoSanitize({
+    replaceWith: '_',
+  }),
+);
 app.use(session(configSession))
 app.use('/', express.static(__dirname + '/public'));
 app.use(express.json());
@@ -45,6 +107,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(morgan('tiny'))
 app.use(methodOverride('_method'));
 app.use(flash())
+
 app.use(passport.initialize())
 app.use(passport.session())
 passport.use(new local(user.authenticate()))
